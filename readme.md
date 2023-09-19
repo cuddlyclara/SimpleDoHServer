140c99dcd3897e60cde68afb133fc19819e9332f
Clara <138817611+cuddlyclara@users.noreply.github.com>
2023-09-19 22:20:25 +0200

    upload to github

---
+++ readme.md
@@ -0,0 +1,43 @@
+# Introduction
+
+This Python3 script serves as a very simple DNS over HTTPS (DoH) server. It should be used behind a reverse proxy, which takes care of the HTTPS configuration.
+
+The goal of this script is to pass the client IP of the requesting client to the DNS server. For this to work, the reverse proxy must pass this information to the script via an HTTP header. Subsequently, this information will be added to the DNS query using the EDNS Client Subnet (ECS). DNS servers such as Pi-hole can be [configured in this way](https://docs.pi-hole.net/ftldns/configfile/#block_edns0_ecs) to process these details in logs or display them in the web interface.
+
+# Why?
+
+The use of Encrypted Client Hello (ECH) often requires the activation of DoH. If a network-wide technology for encrypting all DNS queries is already in use (e.g., [dnscrypt-proxy](https://github.com/DNSCrypt/dnscrypt-proxy)) in conjunction with a DNS-based ad blocker like [Pi-hole](https://pi-hole.net/), it is necessary to set up a local DoH server to avoid the need to bypass any local systems.
+
+# Dependencies
+
+To parse DNS messages, the Python library "dnspython" is used, which can be installed if necessary with the following command:
+
+```bash
+pip3 install dnspython
+```
+
+# Configuration
+
+Settings are defined using variables within the script. By default, the server uses port 8080 on localhost. The DNS server used here is set as an example to the IP address 10.10.10.10 and need to be replaced with the IP address of your local DNS server. Additionally, the header containing the real client IP can be modified as needed.
+
+```python
+# Set the server address, port, dnsserver, and the real IP header
+host = '127.0.0.1'
+port = 8080
+dnsserver = '10.10.10.10'
+realipheader = 'X-Forwarded-For'
+```
+
+# Execution
+
+Since all options are already set within the script, you can easily start the script from the command line:
+
+```bash
+./server.py
+```
+
+A recommended approach is to run this as a service using Systemd.
+
+# Troubleshooting
+
+By default, Pi-hole uses the web server Lighttpd. Up to and including version 1.4.59, the web server responded to empty HTTP headers with a response code of 400. This prevented Firefox from sending requests to the DoH server behind the Lighttpd server due to its default empty "Accept-Encoding" header in DoH requests. This issue was fixed in [this commit](https://github.com/lighttpd/lighttpd1.4/commit/262561f). Unfortunately, older Debian servers cannot update to a Lighttpd version >= 1.4.60. One possible workaround is to set the switch `network.trr.send_empty_accept-encoding_headers` to `false` under `about:config`.
---
+++ server.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import base64
+import logging
+import ipaddress
+import http.server
+import socketserver
+import urllib.parse
+import dns.message
+import dns.edns
+import dns.query
+
+def is_valid_ipv4(ip_str):
+    try:
+        ipaddress.IPv4Address(ip_str)
+        return True
+    except ipaddress.AddressValueError:
+        return False
+
+def requestDNSAnswer(query, clientip):
+    # Parse the DNS query message from wire format
+    request = dns.message.from_wire(query)
+
+    # Log DNS query details: type, domain, and client IP if available
+    if len(request.question) > 0:
+        question = request.question[0]
+        logging.info(f'query {dns.rdatatype.to_text(question.rdtype)} {question.name.to_text()} from {clientip}')
+
+    # Include EDNS with ECS option from client IP address if client IP address is valid
+    if is_valid_ipv4(clientip):
+        ecs = dns.edns.ECSOption.from_text(clientip + '/32')
+        request.use_edns(edns=True, options=[ecs])
+    else:
+        logging.warn(f'client IP {clientip} not valid using server IP for request');
+
+    # Send the query and return the DNS response
+    response = dns.query.udp(request, dnsserver)
+    return response.to_wire()
+
+class DohHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse the dns query parameter
+        query = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        dns_query = query.get('dns', [b''])[0]
+
+        # Respond with DoH response
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/dns-message')
+        self.end_headers()
+        self.wfile.write(requestDNSAnswer(base64.b64decode(dns_query), self.headers[realipheader]))
+
+    def do_POST(self):
+        # Parse input stream
+        content_length = int(self.headers['Content-Length'])
+        dns_query = self.rfile.read(content_length)
+
+        # Respond with DoH response
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/dns-message')
+        self.end_headers()
+        self.wfile.write(requestDNSAnswer(dns_query, self.headers[realipheader]))
+
+# Set the LogLevel to logging.WARNING or logging.ERROR to suppress the output of DNS requests
+logging.basicConfig(level=logging.INFO)
+
+# Set the server address, port, dnsserver and the real ip header
+host = '127.0.0.1'
+port = 8080
+dnsserver = '10.10.10.10'
+realipheader = 'X-Forwarded-For'
+
+# Create the DoH server
+with socketserver.TCPServer((host, port), DohHandler) as httpd:
+    try:
+        print(f'Serving DoH on {host}:{port} using DNS server {dnsserver}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Shutting down the server...')
+        httpd.shutdown()
